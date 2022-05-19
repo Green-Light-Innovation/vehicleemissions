2a29621e99f718be5e073a7357960d1606ba0539
J4CKINS <jackatkins3@gmail.com>
2022-05-19 15:45:03 +0100

    Switched sqlite to mysql DBMS

--- database/engine.py
+++ database/engine.py
@@ -2,25 +2,27 @@ import os
 import sqlite3
 import string
 import random
+import mysql.connector
+
+from config import config
 
 class DatabaseEngine:
 
     connection = None
     cursor = None
-    
-    db_path = "./database/database.db"
-    schema_path = "./database/schema.sql"
 
     @staticmethod
     def connect() -> None:
         """ Connect to the database file """
-
-        # Check if database file exists
-        if not os.path.exists(DatabaseEngine.db_path):
-            DatabaseEngine.create_new_database()
-            return
         
-        DatabaseEngine.connection = sqlite3.connect(DatabaseEngine.db_path) # Create new coneciton object instance
+        DatabaseEngine.connection = mysql.connector.connect(
+            host        = config["DATABASE_SERVER"],
+            port        = config["DATABASE_PORT"],
+            user        = config["DATABASE_USER"],
+            password    = config["DATABASE_PASSWORD"],
+            database    = config["DATABASE"]
+        )
+        
         DatabaseEngine.cursor = DatabaseEngine.connection.cursor() # Create new cursor object instance
 
     @staticmethod
@@ -37,25 +39,6 @@ class DatabaseEngine:
         
         if not DatabaseEngine.connection: return # Dont commit if no connection is established
         DatabaseEngine.connection.commit()
-
-    @staticmethod
-    def create_new_database() -> None:
-        """ Create a new database file (only needed when original file is lost) """
-
-        # Create a new connection
-        # This also creates a new file
-        DatabaseEngine.connection = sqlite3.connect(DatabaseEngine.db_path)
-
-        # Assign new cursor object
-        DatabaseEngine.cursor = DatabaseEngine.connection.cursor()
-
-        # Read the SQL schema
-        with open(DatabaseEngine.schema_path, "r") as file:
-            schema = file.read()
-
-        DatabaseEngine.cursor.executescript(schema) # Execute schema SQL on database
-        DatabaseEngine.commit() # Commit changes
-        DatabaseEngine.disconnect() # Close connection
 
     @staticmethod
     def gen_id() -> str:
